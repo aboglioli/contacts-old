2a14d6e2e0b0fb058ae854b7c728ebd83a8107ec
Alan <alan.boglioli@gmail.com>
2017-09-06 22:31:11 -0300

    Add cors support

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const path = require('path');
 // express
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
